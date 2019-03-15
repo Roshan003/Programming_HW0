4dc300f55fc1747a021567cd0be3aab0d93398f0
Granit <gdedushi5@gmail.com>
2019-03-14 21:51:04 -0400

    Add files via upload
    Added problem 2(prime)

---
+++ Prime.java
@@ -0,0 +1,47 @@
+import java.lang.Thread;
+import java.util.Scanner;
+
+public class Prime extends Thread
+{
+	private int givenNum;
+	
+	public Prime(int givenNum)
+	{
+		this.givenNum = givenNum;
+	}
+	
+	public void SmallerPrimeNumbers() {
+		int count = 0;
+		for (int i = 2; i <= givenNum; i++) {
+
+			if (isPrime(i)) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+	
+	public static boolean isPrime(int n)
+	{
+		for(int i = 2; i < n; i++)
+		{
+			if(n % i == 0)
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Prime number you choose: ");
+		int num = in.nextInt();
+		in.close();
+		Prime p = new Prime(num);
+		p.start();
+		Thread.yield();
+		
+		p.SmallerPrimeNumbers();
+		
+	}
+
+}
